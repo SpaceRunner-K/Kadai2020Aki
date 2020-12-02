a3510def06cbdf8faee27b5b6bf9e7353f03eb8a
SpaceRunner-K <yossiy.github@gmail.com>
2020-12-02 05:57:32 +0000

    課題7途中

--- kadai-06/index2.js
+++ kadai-06/index2.js
@@ -1,21 +1,47 @@
 const str1 = "abc"
 const str2 = "'abc'"
 const str3 = '"abc"'
-
+const str4 = '{"a":undefined}'
+const str5 = '{"a": 123}'
+const str6 = '{a: 123}'
+const str7 = '{"func":()=>console.log("Hi!")}'
 try {
     JSON.parse(str1)    
 } catch (error){
 console.log(error)
-    //JavaScriptで文字列型を取り扱う時は、クオーテーションが必要だから
+    //JavaScriptで文字列型を取り扱う時のクオーテーションとJSONのクオーテーションの2つが必要だから。
 }
 try {
     JSON.parse(str2)
 } catch (error){
-    console.log('JSON.parseがうまく実行出来ません。コメントを参照してください。')
+    console.log(error)
+    //JSONで文字列を扱う時は、シングルクオーテーションではなくダブルクオーテーションが必要
 }
     try {
     JSON.parse(str3)
 } catch (error){
-    console.log('JSON.parseがうまく実行出来ません。コメントを参照してください。')
+    console.log(error)
+}
+    try {
+    JSON.parse(str4)
+} catch (error){
+    console.log(error)
+    //underfindは値として使えないから
     }
-    
+     try {
+    JSON.parse(str5)
+} catch (error){
+    console.log(error)
+    }
+    try {
+    JSON.parse(str6)
+} catch (error){
+    console.log(error)
+    //文字列を扱う時はダブルクオーテーションを使う
+    }
+    try {
+    JSON.parse(str7)
+} catch (error){
+    console.log(error)
+    //JSPNで関数は扱えないため
+    }
